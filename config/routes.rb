cade25e39eca2b867b9dbd5ba5568495da5c1357
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-04-05 13:23:29 +0000

    10章

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
-  get '/about', to: 'homes#about'
+  devise_for :users
+  
+  resources :post_images, only: [:new, :create, :index, :show]
+  
+  get "/homes/about" => "homes#about", as: "about"
 end
---
+++ test/controllers/post_image_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class PostImageControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get post_image_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get post_image_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get post_image_show_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get post_image_destroy_url
+    assert_response :success
+  end
+end
