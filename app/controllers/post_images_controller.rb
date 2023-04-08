17f4e00de593ef1bd42d7b4cf8fbe02c047a85b3
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-04-08 02:14:30 +0000

    21章　バリデーションまで

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,12 +16,23 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
     redirect_to "/post_images"
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      redirect_to_post_image_path
+    else
+      render :new
+    end
   end
 
   private
