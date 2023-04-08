50d8f5cf957ee8ce8cca43ca401e3cdca22e7214
Shinji <117142484+takenokonosato1969@users.noreply.github.com>
2023-04-08 06:40:10 +0000

    24章終わり　完成？

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,15 +3,9 @@ class PostImagesController < ApplicationController
     @post_image =PostImage.new
   end
 
-  def create
-    @post_image = PostImage.new(post_image_params)
-    @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
-  end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
@@ -29,7 +23,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     if @post_image.save
-      redirect_to_post_image_path
+      redirect_to post_images_path
     else
       render :new
     end
